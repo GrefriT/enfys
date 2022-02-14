e2b320bf66fce9723f3d31c2b0d46a528ae31f39
Alexander Skudnov <xmxxx67@gmail.com>
2022-02-14 16:38:41 +0200

    feat(frontend): rain animation

--- packages/frontend/layouts/index.tsx
+++ packages/frontend/layouts/index.tsx
@@ -1,13 +1,14 @@
+import styles from "styles/rain.module.css";
 import type { ReactNode } from "react";
 import Navbar from "components/Navbar";
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
 	return (
-		<>
+		<div className={`${styles.rain} min-h-screen`}>
 			<div className="container mx-auto px-4 pb-8 space-y-12">
 				<Navbar />
 				{children}
 			</div>
-		</>
+		</div>
 	);
 }
